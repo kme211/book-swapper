1d12a897f7e5cda480493d3ad7761ab86167f4cd
Keari Eggers <kungfu.keari@gmail.com>
2016-08-31 13:00:09 -0500

    fix: mislabeled import

--- src/routes.js
+++ src/routes.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Route, IndexRoute } from 'react-router';
 import App from './components/App';
 import HomePage from './components/home/HomePage';
-import AboutPage from './components/account/AccountPage';
+import AccountPage from './components/account/AccountPage';
 
 export default (
   <Route path="/" component={App}>
