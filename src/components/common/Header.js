0be464ec29868c306dcf19d8607501ee006c462c
Keari Eggers <kungfu.keari@gmail.com>
2016-09-01 13:46:13 -0500

    make menu a list

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -3,12 +3,18 @@ import { Link, IndexLink } from 'react-router';
 
 const Header = () => {
   return (
-    <header>
-      <h1>Book Swapper</h1>
-      <nav role="navigation">
-        <IndexLink to="/" activeClassName="active">Home</IndexLink>
-        <Link to="account" activeClassName="active">Account</Link>
-      </nav>
+    <header className="main-header">
+      <div className="main-header__inner">
+        <h1 className="main-header__title">Book Swapper</h1>
+        <ul className="main-header__menu" role="navigation">
+          <li className="main-header__menu-item-outer">
+            <IndexLink to="/" activeClassName="active" className="main-header__menu-item">Home</IndexLink>
+          </li>
+          <li className="main-header__menu-item-outer">
+            <Link to="account" activeClassName="active" className="main-header__menu-item">Account</Link>
+          </li>
+        </ul>
+      </div>
     </header>
   );
 };
