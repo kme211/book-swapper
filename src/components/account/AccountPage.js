5d2bfbeaad7f7460ebe8629808ea34be8c1b42a1
Keari Eggers <kungfu.keari@gmail.com>
2016-09-29 16:36:21 -0500

    add groups and books to account page

--- src/components/account/AccountPage.js
+++ src/components/account/AccountPage.js
@@ -11,21 +11,42 @@ class AccountPage extends React.Component {
       <div className="container">
         <h2 className="sub-header">Account</h2>
         <div className="account-info">
-          <p className="account-info__item"><span className="account-info__label">Name:</span> {this.props.user.firstName + ' ' + this.props.user.lastName}</p>
-          <p className="account-info__item"><span className="account-info__label">Email:</span> {this.props.user.email}</p>
-        </div>
+          <div className="account-info__item">
+            <span className="account-info__label">Name:</span> {this.props.user.firstName + ' ' + this.props.user.lastName}
+          </div>
+          <div className="account-info__item">
+            <span className="account-info__label">Email:</span> {this.props.user.email}
+          </div>
+          <div className="account-info__item">
+            <span className="account-info__label">My groups: </span>
+            <ul className="account-info__groups">{this.props.groups.map((group, index) => <li className="account-info__group" key={index}>{group.name}</li>)}</ul>
+          </div>
+          <div className="account-info__item">
+            <span className="account-info__label">My books: </span>
+            <ul className="account-info__books">
+              {this.props.user.books && this.props.user.books.map((book, index) => <li className="account-info__book" key={index}>{book.title}</li>)}
+            </ul>
+          </div>
+      </div>
       </div>
     );
   }
 }
 
 AccountPage.propTypes = {
-  user: React.PropTypes.object.isRequired
+  user: React.PropTypes.object.isRequired,
+  groups: React.PropTypes.array.isRequired
+};
+
+AccountPage.defaultProps = {
+  user: {},
+  groups: []
 };
 
 function mapStateToProps(state) {
   return {
-    user: state.user
+    user: state.user,
+    groups: state.groups
   };
 }
 
