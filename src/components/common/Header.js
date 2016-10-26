3a877fdde823dd7afa5d8535bdc801b33abb6d98
Keari Eggers <kungfu.keari@gmail.com>
2016-10-26 15:20:47 -0500

    dry link style

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -1,7 +1,7 @@
 import React, {PropTypes} from 'react';
 import { Link, IndexLink } from 'react-router';
 import Heading from 'components/atoms/Heading';
-import styled from 'styled-components';
+import styled, {css} from 'styled-components';
 import {colors, fonts} from 'components/globals';
 
 const Wrapper = styled.header`
@@ -18,7 +18,7 @@ const LinkWrapper = styled.li`
   display: inline-block;
 `;
 
-const StyledIndexLink = styled(IndexLink)`
+const linkStyle = css`
   font-family: ${fonts.primary};
   display: block;
   text-decoration: none;
@@ -38,24 +38,12 @@ const StyledIndexLink = styled(IndexLink)`
   }
 `;
 
+const StyledIndexLink = styled(IndexLink)`
+  ${linkStyle}
+`;
+
 const StyledLink = styled(Link)`
-  font-family: ${fonts.primary};
-  display: block;
-  text-decoration: none;
-  color: ${colors.primary[0]};
-  padding: 1rem;
-  border-bottom: 2px solid white;
-  transition: all 0.4s ease;
-
-  &:hover {
-    background-color: #F8F8F8;
-    border-bottom: 2px solid ${colors.secondary[0]};
-  }
-
-  &.active {
-    font-weight: bolder;
-    color: ${colors.secondary[0]};
-  }
+  ${linkStyle}
 `;
 
 const Header = () => {
