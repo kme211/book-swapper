ee9aafe24926802bab347b8ad2e27706606dacda
Keari Eggers <kungfu.keari@gmail.com>
2016-10-26 12:15:56 -0500

    add inline styles

--- src/components/books/FilterTag.js
+++ src/components/books/FilterTag.js
@@ -1,16 +1,22 @@
 import React, { PropTypes } from 'react';
+import styled from 'styled-components';
+import { colors, fonts } from 'components/globals';
 
-const FilterTag = (props) => {
+const Wrapper = styled.li`
+  font-family: ${fonts.primary};
+  background-color: ${props => props.active ? '#44C662' : 'white'};
+  border-radius: 6px;
+  margin: 2px 2px 40px 2px;
+  padding: 3px 8px;
+  display: inline-block;
+  color: ${props => props.active ? 'white' : colors.grayscale[1]};
+  border: ${props => props.active ? '1px solid #E6E6E6' : '1px dashed ' + colors.grayscale[1]};
+  cursor: pointer;
+`;
 
-  let checkBoxClass = "filter__tag";
-  if(props.active) {
-    checkBoxClass += ' active';
-  }
-
-  return (
-    <li className={checkBoxClass} onClick={props.onClick}>{props.tag}</li>
-  );
-};
+const FilterTag = (props) => (
+  <Wrapper active={props.active} onClick={props.onClick}>{props.tag}</Wrapper>
+);
 
 FilterTag.propTypes = {
   tag: React.PropTypes.string.isRequired,
