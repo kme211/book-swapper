ebb036dd5b8b15c3d518fd18b429adb0428277cb
Keari Eggers <kungfu.keari@gmail.com>
2016-10-27 11:42:07 -0500

    fix: 0 delay when testing

--- src/api/delay.js
+++ src/api/delay.js
@@ -1,1 +1,6 @@
-export default 250;
+if (process.env.NODE_ENV === 'test') {
+  module.exports = 0;
+  // Using require instead of import because dynamic imports aren't supported by ES6
+} else {
+  module.exports = 1000;
+}
